1770cbf9306fc367e58bb69578a28d5590817cf6
Wambasi <wambasivin@gmail.com>
2024-02-22 11:14:24 +0300

    user input ssignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# user_input.py
+# Ask for user to input their details
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Prints out a personalized message
+print(f"Hello {name}you are {age} years old and live in {location} ")
